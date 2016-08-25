c99e967cec67b484a96dac4632e1dbba1540f5d9
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-25 22:58:22 +0100

    Adds endorsements

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ActiveRecord::Base
 
   belongs_to :restaurant
   belongs_to :user
+  has_many :endorsements
 
   validates :user, uniqueness: { scope: :restaurant,
             message: "has reviewed this restaurant already" }
